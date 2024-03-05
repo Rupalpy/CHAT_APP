064026b4188ffbf9c3fbdb0e1fc8b75eed38e178
Rupal Patel Yadav <rpy8418@gmail.com>
2024-03-05 20:39:55 +0530

    get users for sidebar endpoint

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import authRoutes from "./routes/auth.route.js";
-
-import messageRoutes from "./routes/message.route.js";
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app= express();
@@ -11,8 +11,10 @@ const PORT=process.env.PORT || 5000;
 dotenv.config();
 app.use(express.json()); // to parse the incoming request with JSON payloads from(req.body)
 app.use(cookieParser());
+
 app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
+app.use("/api/users",userRoutes);
 
 app.get("/",(req,res)=>{
     res.send("server is ready");
